b8c7e5eaa8e84a1b9964061d1bca20a1f3e44cb8
Boris SCHAPIRA <borisschapira@gmail.com>
2017-06-04 16:55:56 +0200

    Add theme --skip-ci

---
+++ themes/bs-theme/README.md
@@ -0,0 +1,17 @@
+# bs-theme
+
+bs-theme is a really minimal blog theme for hugo.
+
+## Getting Started
+
+Clone this repository to your hugo theme directory.
+
+```
+mkdir themes
+cd themes
+git clone https://github.com/borisschapira/bs-theme.git
+```
+
+## License
+
+bs-theme is licensed under the [MIT License](LICENSE.md).
---
+++ themes/bs-theme/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sassFiles = ['sass/*.sass','sass/*.scss'];
+gulp.task("sass", function () {
+  gulp.src(sassFiles)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('static/css/'));
+});
+
+gulp.task("watch", function () {
+  gulp.watch(sassFiles, ['sass']);
+});
+
+gulp.task('default', ['sass']);
+
+
